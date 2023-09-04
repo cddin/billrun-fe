05bd083398a930c042e52f7f5062500c3845cff5
Cddin <cddin80@gmail.com>
2023-09-05 04:04:03 +0800

    add iframe

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -56,7 +56,7 @@ class Dashboard extends Component {
           width="600"
           height="450"
           src="https://lookerstudio.google.com/embed/reporting/43f28cff-8cfd-4e39-bd28-cbf98d2612bb/page/GNJaD"
-          // frameborder="0"
+          frameborder="0"
           // style="border:0"
           // allowfullscreen
         ></iframe>
