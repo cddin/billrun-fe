9d54992c0b7819b55750a8e12916f83eb5dc5a49
Cddin <cddin80@gmail.com>
2023-09-05 04:11:18 +0800

    add iframe

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -53,11 +53,11 @@ class Dashboard extends Component {
           <RenderPieChart />
         </Panel> */}
         <iframe
-          width="600"
-          height="450"
+          width="100%"
+          height="100vh"
           src="https://lookerstudio.google.com/embed/reporting/43f28cff-8cfd-4e39-bd28-cbf98d2612bb/page/GNJaD"
           frameborder="0"
-          style="width:100%; height:100vh"
+
           // allowfullscreen
         ></iframe>
         {/* <iframe src="https://www.youtube.com/embed/uXWycyeTeCs"></iframe> */}
